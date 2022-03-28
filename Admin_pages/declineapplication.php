eb782980ff6362b4774fc0825b61e28b25da6b05
Ailene Advincula <90735622+aiuaaiee@users.noreply.github.com>
2022-03-28 09:52:20 +0800

    Delete Admin_pages directory

--- Admin_pages/declineapplication.php
+++
@@ -1,8 +0,0 @@
-<?php
-	
-    include 'dbcon.php';  
-    $ID= $_GET['ID'];
-    $result = $mysqli->query("DELETE FROM teachregs WHERE id = '".$ID."'");
-   
-	header('location:applications.php');
-?>	
